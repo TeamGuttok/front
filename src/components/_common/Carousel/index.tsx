a8afcb57cd8a9b25bd654fe72c8157caec776643
stillcorners <yheedev@gmail.com>
2025-02-01 23:50:10 +0900

    feat: 세션 여부에 따라 메인 페이지 다르게 렌더링 / refactor: 캐러샐 완성, /add와 /add/detail 반응형 UI 수정 및 접근성을 고려한 속성 추가

---
+++ src/components/_common/Carousel/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { KNOWN_SERVICES } from '#constants/knownServices'
+import { Card } from '../Card'
+
+export default function Carousel() {
+  const duplicatedServices = [...KNOWN_SERVICES, ...KNOWN_SERVICES]
+
+  return (
+    <div className="overflow-hidden w-full relative mt-2">
+      <div className="flex animate-scroll w-[max-content] min-w-full">
+        {duplicatedServices.map((service, index) => (
+          <Card
+            key={`${service.id}-${index}`}
+            className="flex-shrink-0 w-64 h-40 background-color-[hsl(var(--muted-foreground))] shadow-lg rounded-lg flex flex-col justify-center items-center mx-2"
+          >
+            <div
+              className="w-16 h-16 mb-2 bg-center bg-no-repeat bg-contain"
+              style={{
+                backgroundImage: `url(${service.iconUrl})`,
+              }}
+            />
+            <span className="text-lg font-semibold ">{service.name}</span>
+          </Card>
+        ))}
+      </div>
+    </div>
+  )
+}
