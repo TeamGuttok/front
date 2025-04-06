373aaa29e442a0aeb9d0f0cbee70d2cfe06fc623
yheedev <yheedev@gmail.com>
2025-04-07 00:15:43 +0900

    fix: postcss 충돌 해결중

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+const tailwindcss = require('@tailwindcss/postcss')
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+// import tailwindcss from 'tailwindcss'
+// import autoprefixer from 'autoprefixer'
+
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+//   plugins: [tailwindcss(), autoprefixer()],
+// }
+
+// export default config
