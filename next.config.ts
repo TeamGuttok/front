5c5a55ca441c30969f03bc60266f832f16ca35ad
yheedev <yheedev@gmail.com>
2025-05-07 04:27:13 +0900

    chore: tsconfig, next.config 수정

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next'
 import path from 'path'
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   webpack: (config: any) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
