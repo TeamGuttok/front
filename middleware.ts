4871b44a636067923633829a70d26b8cb0a49d77
yheedev <yheedev@gmail.com>
2025-05-06 16:18:04 +0900

    fix: auth, protected에 따라 경로 구분

--- middleware.ts
+++ middleware.ts
@@ -1,49 +1,30 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-const PUBLIC_PATHS = ['/', '/login', '/register', '/forgotPassword']
-
+export const config = {
+  matcher: ['/mypage', '/mypage/:path*', '/notification', '/item/:path*'],
+}
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl
+  const session = request.cookies.get('SESSION')
+  const pathname = request.nextUrl.pathname
 
-  if (
-    PUBLIC_PATHS.includes(pathname) ||
-    pathname.startsWith('/_next') ||
-    pathname.startsWith('/static') ||
-    pathname.startsWith('/images/favicon') ||
-    pathname.startsWith('/favicon.png')
-  ) {
-    return NextResponse.next()
-  }
+  const isApi = pathname.startsWith('/api')
 
-  const isLoggedIn = request.cookies.has('SESSION') // 또는 'auth'
-  if (!isLoggedIn) {
-    return NextResponse.redirect(new URL('/login', request.url))
+  if (!session) {
+    if (isApi) {
+      return Response.json(
+        { success: false, message: '로그인이 필요합니다.' },
+        { status: 401 },
+      )
+    } else {
+      return NextResponse.redirect(new URL('/login', request.url))
+    }
   }
 
   return NextResponse.next()
 }
 
-// Dynamic Route 예외처리
-//   const isItemDetailOrEdit = /^\/item\/\d+\/(detail|edit)$/.test(pathname)
-//   if (isItemDetailOrEdit) {
-//     const authCookie = request.cookies.get('SESSION')
-//     if (!authCookie) {
-//       const loginUrl = new URL('/login', request.url)
-//       return NextResponse.redirect(loginUrl)
-//     }
-//     return NextResponse.next()
-//   }
-
-//   const authCookie = request.cookies.get('SESSION')
-//   if (!authCookie) {
-//     const loginUrl = new URL('/login', request.url)
-//     return NextResponse.redirect(loginUrl)
-//   }
-
-//   return NextResponse.next()
-// }
-
-export const config = {
-  matcher: ['/((?!_next/static|_next/image).*)'],
-}
+// export const config = {
+//   matcher: [
+//     '/((?!_next/static|_next/image|images/favicon|login|register|forgotPassword).*)',
+//   ],
