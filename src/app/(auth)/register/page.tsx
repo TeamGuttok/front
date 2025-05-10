6958c0378d3d31b92188c95ab9fe163c158f633d
yheedev <yheedev@gmail.com>
2025-05-10 17:48:03 +0900

    refactor: 회원가입 페이지 및 zod 검증, 에러 ui 추가

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -11,12 +11,15 @@ import { registerSchema } from '#schema/userSchema'
 import { useRegister } from '#apis/authClient'
 import { useRouter } from 'next/navigation'
 import { PATH } from '#app/routes'
+import CardTitle from '#components/_common/CardTitle'
+import { SelectLabel, SelectGroup } from '#components/_common/Select'
+import { groupClassName, labelClassName, inputClassName } from '#style/style'
+import { cn } from '#components/lib/utils'
 
 //const RegisterSuccess = dynamic(() => import('./success/page'))
 
 export default function Register() {
-  const { user, setUser } = useAuthStore()
-  const isLoggedIn = useAuthStore((state) => state.isLoggedIn)
+  const { user, setUser, isEmailVerified } = useAuthStore()
   const [password, setPassword] = useState<string>('')
   const [passwordConfirm, setPasswordConfirm] = useState<string>('')
   const [error, setError] = useState<Record<string, string[]>>({})
@@ -41,6 +44,14 @@ export default function Register() {
       return
     }
 
+    if (!isEmailVerified) {
+      setError((prev) => ({
+        ...prev,
+        email: ['이메일 인증을 완료해 주세요.'],
+      }))
+      return
+    }
+
     registerUser(
       {
         email: input.email,
@@ -60,92 +71,101 @@ export default function Register() {
   }
 
   return (
-    <div className="flex flex-col items-center sm:m-auto sm:-translate-y-12">
-      <div className="w-full sm:w-[60vw] sm:max-w-[832px] sm:p-8 sm:rounded-md sm:border sm:border-border">
-        <div className="flex flex-col justify-center items-center">
-          <div className="flex flex-col items-center w-full mt-5">
-            <span className="text-3xl font-bold ">회원가입</span>
-          </div>
-          <div className="w-full h-[1px] bg-border mt-5"></div>
-
-          <form
-            onSubmit={handleSubmit}
-            className="flex flex-col w-full max-w-lg mt-10 px-10"
-          >
-            <div className="flex flex-col gap-1 min-h-16">
-              <div className="flex items-center">
-                <Label htmlFor="nickname" className="w-14 mr-6">
-                  <span className="text-base font-medium">닉네임</span>
-                </Label>
-                <Input
-                  name="nickname"
-                  placeholder="닉네임을 입력하세요"
-                  className="w-0 grow"
-                  value={user?.nickName ?? ''}
-                  onChange={(e) => setUser({ nickName: e.target.value })}
-                />
-              </div>
-              <ErrorMessage errors={error?.nickname} className="ml-20" />
-            </div>
-
-            <RegisterInputField
-              email={user?.email ?? ''}
-              onChangeEmail={(e) => setUser({ ...user, email: e.target.value })}
-            />
-
-            <div className="flex flex-col gap-1 min-h-16">
-              <div className="flex items-center">
-                <Label htmlFor="password" className="w-[3.46rem] mr-6">
-                  <span className="text-base font-medium">비밀번호</span>
-                </Label>
-                <Input
-                  name="password"
-                  type="password"
-                  placeholder="비밀번호를 입력하세요"
-                  className="w-0 grow"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  autoComplete="new-password"
-                />
-              </div>
-              <ErrorMessage errors={error?.password} className="ml-20" />
-            </div>
-
-            <div className="flex flex-col gap-1 min-h-16">
-              <div className="flex items-center">
-                <Label
-                  htmlFor="password-confirm"
-                  className="w-[3.46rem] mr-6 text-center"
-                >
-                  <span className="text-base font-medium leading-5">
-                    비밀번호 <br /> 확인
-                  </span>
-                </Label>
-                <Input
-                  name="password-confirm"
-                  type="password"
-                  placeholder="비밀번호를 입력하세요"
-                  className="w-0 grow"
-                  value={passwordConfirm}
-                  onChange={(e) => setPasswordConfirm(e.target.value)}
-                  autoComplete="new-password"
-                />
-              </div>
-              <ErrorMessage
-                errors={error?.passwordConfirm || []}
-                className="ml-20"
-              />
-            </div>
-            <Button
-              type="submit"
-              className="flex justify-self-center w-full h-10 text-md rounded-lg mt-10"
-              disabled={isRegistering}
-            >
-              회원가입
-            </Button>
-          </form>
-        </div>
+    <>
+      <div className="flex flex-col items-center w-full">
+        <h1 className="text-3xl sm:text-3xl font-bold ">회원가입</h1>
       </div>
-    </div>
+      <div className="w-full h-[1px] bg-border mt-5"></div>
+
+      <div className="w-full p-5">
+        <form onSubmit={handleSubmit} className="grid grid-cols-1 gap-1.5">
+          <SelectGroup className={cn(groupClassName, 'mt-5 mb-3')}>
+            <SelectLabel
+              aria-labelledby="registerNickname"
+              aria-describedby="registerNickname-required"
+              aria-required="true"
+              className={cn(labelClassName, 'w-[3.46rem] mr-12')}
+            >
+              닉네임
+            </SelectLabel>
+            <Input
+              name="nickname"
+              type="text"
+              aria-labelledby="registerNickname"
+              aria-describedby="registerNickname-required"
+              placeholder="닉네임을 입력하세요"
+              value={user?.nickName ?? ''}
+              onChange={(e) => setUser({ nickName: e.target.value })}
+              className="grow"
+            />
+          </SelectGroup>
+          <ErrorMessage errors={error?.nickName} className="ml-20" />
+
+          <RegisterInputField
+            email={user?.email ?? ''}
+            onChangeEmail={(e) => setUser({ ...user, email: e.target.value })}
+            errorEmail={error?.email}
+          />
+
+          <SelectGroup className={cn(groupClassName, 'mt-5')}>
+            <SelectLabel
+              aria-labelledby="registerPassword"
+              aria-describedby="registerPassword-required"
+              aria-required="true"
+              className={cn(labelClassName, 'w-[3.46rem] mr-12')}
+            >
+              비밀번호
+            </SelectLabel>
+            <Input
+              name="password"
+              type="password"
+              aria-labelledby="mypagePassword"
+              aria-describedby="mypagePassword-required"
+              placeholder="비밀번호를 입력하세요"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              autoComplete="new-password"
+              className="grow"
+            />
+          </SelectGroup>
+          <ErrorMessage errors={error?.password} className="ml-20" />
+
+          <SelectGroup className={cn(groupClassName, 'mt-5')}>
+            <SelectLabel
+              aria-labelledby="registerPasswordConfirm"
+              aria-describedby="registerPasswordConfirm-required"
+              aria-required="true"
+              className={cn(labelClassName, 'w-[3.46rem] mr-12')}
+            >
+              <span className="leading-5">
+                비밀번호 <br /> 확인
+              </span>
+            </SelectLabel>
+            <Input
+              name="passwordConfirm"
+              type="password"
+              placeholder="비밀번호를 입력하세요"
+              className="grow"
+              value={passwordConfirm}
+              onChange={(e) => setPasswordConfirm(e.target.value)}
+              autoComplete="new-password"
+            />
+          </SelectGroup>
+          <ErrorMessage
+            errors={error?.passwordConfirm || []}
+            className="ml-20"
+          />
+          <Button
+            type="submit"
+            className="flex justify-self-center w-full h-10 text-md rounded-lg mt-10"
+            disabled={isRegistering}
+          >
+            회원가입
+          </Button>
+        </form>
+      </div>
+      {/* </div> */}
+      {/* </div> */}
+    </>
   )
 }
