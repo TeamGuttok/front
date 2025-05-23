b1b7d0e31892f4a6265104f7db724309fc88c0e8
yheedev <yheedev@gmail.com>
2025-05-24 00:16:24 +0900

    refactor: 결제수단 placeholder 추가, 상세정보/수정 페이지에서 일괄적인 font-weight 적용

--- src/components/_common/Select/index.tsx
+++ src/components/_common/Select/index.tsx
@@ -25,12 +25,6 @@ const SelectTrigger = React.forwardRef<
     )}
     {...props}
   >
-    {/* <span className="text-sm sm:text-base mr-2">{children}</span>
-      "flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
-      className
-    )}
-    {...props}
-  > */}
     <span className="text-sm sm:text-base mr-2">{children}</span>
     <SelectPrimitive.Icon asChild>
       <ChevronDown className="h-4 w-4 opacity-50" />
