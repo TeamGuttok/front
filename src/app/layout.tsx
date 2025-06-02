639eddd2556661819da6bf322ee8dbcacc111ad7
yheedev <yheedev@gmail.com>
2025-06-02 18:40:31 +0900

    feat: SEO 메타데이터 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,66 @@ import { Toaster } from '#components/_common/Toast/toaster'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: '구똑',
+  title: {
+    default: '구똑',
+    template: '%s | 구똑',
+  },
   description: '구똑: 구독을 똑똑하게',
+  keywords: [
+    '구똑',
+    '구독',
+    '구독관리',
+    '구독 관리',
+    '구독 관리 서비스',
+    '구독을 똑똑하게',
+    '구독 관리 앱',
+    '구독관리',
+    '고정 지출 관리',
+    '고정지출',
+    '고정 지출',
+    '정기결제',
+    '정기 결제',
+    '가계부',
+    '구독 서비스',
+    'guttok',
+    'Guttok',
+    'Subscription',
+    'Subscription Manager',
+  ],
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-snippet': -1,
+      'max-image-preview': 'large',
+      'max-video-preview': -1,
+    },
+  },
+  alternates: {
+    canonical: 'https://guttok.site',
+  },
+
+  openGraph: {
+    title: '구똑 | 구독을 똑똑하게',
+    description:
+      '고정 지출을 한눈에 관리하고, 알림을 받을 수 있는 스마트한 구독 관리 앱, 구똑.',
+    url: 'https://guttok.site',
+    siteName: '구똑',
+    images: [
+      {
+        url: '/images/favicon/light_favicon.png',
+        width: 1200,
+        height: 630,
+        alt: '구독을 똑똑하게, 구똑',
+      },
+    ],
+    locale: 'ko_KR',
+    type: 'website',
+  },
+
   icons: {
     icon: [
       {
