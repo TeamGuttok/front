32619bc9b951c7c569cead9cdc17e9cc87384924
yheedev <yheedev@gmail.com>
2025-06-17 20:10:54 +0900

    fix: params 사용 수정

--- src/app/(protected)/item/[id]/detail/page.tsx
+++ src/app/(protected)/item/[id]/detail/page.tsx
@@ -4,21 +4,22 @@ import ClientDetailView from './ClientDetailView'
 import { getItemById } from '#apis/subscriptionAPI'
 import type { PageProps } from '#types/subscription'
 
-export default async function ItemDetailView({
-  params,
-}: PageProps<{ id: string }>) {
-  const { id } = params
+export default async function ItemDetailView(props: {
+  params: Promise<{ id: string }>
+}) {
+  // const { id } = params
+  const params = await props.params
 
-  if (!id || isNaN(Number(id))) {
-    return (
-      <p className="text-center text-gray-500">
-        잘못된 접근입니다. 유효한 구독 항목 ID를 제공해주세요.
-      </p>
-    )
-  }
+  // if (!id || isNaN(Number(id))) {
+  //   return (
+  //     <p className="text-center text-gray-500">
+  //       잘못된 접근입니다. 유효한 구독 항목 ID를 제공해주세요.
+  //     </p>
+  //   )
+  // }
 
   try {
-    const detailView = await getItemById(id)
+    const detailView = await getItemById(params.id)
     return <ClientDetailView initialData={detailView} params={params} />
   } catch (err) {
     console.error('SSR 유저 정보 로드 실패:', err)
